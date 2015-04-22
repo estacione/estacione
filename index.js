6589886f35cbd90834c3b9d66c740457b76e6846
estacione@github.com <estacionefaccat@gmail.com>
2015-04-22 00:26:17 -0300

    arquivos do exemplo

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-//var express = require('express');
-//var app = express();
-var http = require('http');
+var express = require('express');
+var app = express();
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Estacione\n');
-}).listen(1337, '127.0.0.1');
-
-/*
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
@@ -18,4 +11,3 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
-*/
