a54fe39b935effff145b671d9a3a3ccdc9fc3ee7
estacione@github.com <estacionefaccat@gmail.com>
2015-04-22 21:31:33 -0300

    teste

--- app.js
+++ app.js
@@ -8,4 +8,4 @@ http.createServer(function (req, res) {
 }).listen(5000, '127.0.0.1');
 
 
-console.log('Server running at http://127.0.0.1:1337/');
+console.log('Server running at http://127.0.0.1:5000/');
